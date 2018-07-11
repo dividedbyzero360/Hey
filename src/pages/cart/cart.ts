0c37354beaff19e404ac7336dd345cedc9f9caef
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-07-11 01:27:40 -0400

    Cart count incremet decrement function

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -7,7 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class CartPage {
 
+  count1:number=1;
+  count2:number;
+  count3:number;
   constructor(public navCtrl: NavController) {
+    this.count1=1;
+    this.count2=1;
+    this.count3=1;
+  }
+
+    incrementCount(id:number): void{
+    if(id==1){
+      this.count1++;
+    }
+    if(id==2){
+      this.count2++;
+    }
+    if(id==3){
+      this.count3++;
+    }
+  }
+
+  decementCount(id:number): void{
+    if(id==1){
+      this.count1--;
+    }
+    if(id==2){
+      this.count2--;
+    }
+    if(id==3){
+      this.count3--;
+    }
   }
   
 }
